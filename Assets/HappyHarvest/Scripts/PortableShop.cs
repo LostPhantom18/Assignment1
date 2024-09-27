4dd32332f5704e4442bb8042e1c4b7b341f4b156
LostPhantom18 <Jonah_Bron@Outlook.com>
2024-09-27 10:30:24 -0400

    Added Portable shop functionality
    Jonah Added the Portable shop functionality

---
+++ Assets/HappyHarvest/Scripts/PortableShop.cs
@@ -0,0 +1,12 @@
+using HappyHarvest;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortableShop : MonoBehaviour
+{
+    public void OnClick()
+    {
+        UIHandler.OpenMarket();
+    }
+}
